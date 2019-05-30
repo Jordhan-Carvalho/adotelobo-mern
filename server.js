ca81d9665be22fb2a114bc6a30fe1e5e8699d780
Jordhan Carvalho <jordhan.rdz@gmail.com>
2019-05-30 14:35:43 -0300

    Prepared for deployment Heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -10,15 +11,21 @@ connectDB();
 // BodyParsers now included on express, so instead of using it, we now use
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API Running');
-});
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/animals', require('./routes/api/animals'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
